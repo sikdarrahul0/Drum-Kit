aef35efd50ed7d650c3788e8c8aa936789e0a21f
sikarrahul0 <sikdarrahul0@gmail.com>
2020-08-01 00:24:37 +0600

    project done

---
+++ index.js
@@ -0,0 +1,56 @@
+var buttonList = document.querySelectorAll('button');
+for(var i = 0; i < buttonList.length; i++){
+    buttonList[i].addEventListener('click',function(){
+        makeSound(this.innerHTML)
+        buttonAnimation(this.innerHTML)
+    })
+}
+document.addEventListener('keypress', function(event){
+    var symbol = event.key;
+    makeSound(symbol)
+    buttonAnimation(symbol)
+})
+
+function makeSound(key){
+switch (key) {
+    case "w":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+    case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+    case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+    case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;  
+    case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;  
+    case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break; 
+    case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break; 
+    default:
+            console.log(key);
+            break;
+}
+}
+function buttonAnimation(key){
+    var activeButtion = document.querySelector("." + key);
+    activeButtion.classList.add("pressed");
+    
+    setTimeout(function(){
+        activeButtion.classList.remove("pressed");
+    },100)
+}
